fc215561cd913050739a610a7fcd78bbfbcc23a8
AndreTyrebrant <angu0098@student.hv.se>
2025-03-06 10:03:29 +0100

    La in CleaningService
    MVC projekt som ska kopplas till CleaningAPI

---
+++ Grupp4 - Git/CleaningService/CleaningService/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics;
+using CleaningService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleaningService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //----------------------------------------------------------------------------
+
+        public async Task<IActionResult> fetchAPI()
+        {
+            List<CleaningAPI>? tasksAPI = null;
+
+            var client = new HttpClient();
+
+            // Creates a http request and changes the URI to the correct localhost.
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://localhost:"),
+            };
+
+            try
+            {
+                // Sends request to the API and puts the json response to the List
+                using (var response = await client.SendAsync(request))
+                {
+                    tasksAPI = await response.Content.ReadFromJsonAsync<List<CleaningAPI>>();
+                }
+            }
+            catch (Exception e)
+            {
+                // Gives an error if the API is not available
+                _logger.LogError(e.Message);
+            }
+
+            // Returns the result to the View for rendering
+            return View(tasksAPI);
+        }
+
+        //-----------------------------------------------------------------------------
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Grupp4 - Git/CleaningService/CleaningService/Models/CleaningAPI.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CleaningService.Models
+{
+    public class CleaningAPI
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public int StaffId { get; set; }
+        public bool IsCleaning { get; set; }
+        public string? ReportedDamages { get; set; }
+    }
+}
