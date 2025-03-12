ad084ecb46c7a52500e35c4733ab1f2e714c2620
AndreTyrebrant <angu0098@student.hv.se>
2025-03-12 10:59:35 +0100

    Lägger till uppdaterad CleaningService

--- Grupp4 - Git/CleaningService/CleaningService/Controllers/HomeController.cs
+++ Grupp4 - Git/CleaningService/CleaningService/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace CleaningService.Controllers
             return View();
         }
 
+        public IActionResult Clean()
+        {
+            return View();
+        }
+
         //----------------------------------------------------------------------------
 
         public async Task<IActionResult> fetchAPI()
