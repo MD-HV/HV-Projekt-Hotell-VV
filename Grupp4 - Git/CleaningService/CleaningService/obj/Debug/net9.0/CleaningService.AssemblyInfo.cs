fba13cede03c670ca28cadb3bee7197e0fe5847e
MD-HV <mida0022@student.hv.se>
2025-03-14 10:11:50 +0100

    nice

--- Grupp4 - Git/CleaningService/CleaningService/obj/Debug/net9.0/CleaningService.AssemblyInfo.cs
+++ Grupp4 - Git/CleaningService/CleaningService/obj/Debug/net9.0/CleaningService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleaningService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269f44456671cf7f990e8b52e4a19f357d3a4802")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c57799ebd5f4aa88d817fa9bc07c2d00ef9ad53")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleaningService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleaningService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
